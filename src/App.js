fb2fdb02a277ecb3fa8a71e9c9db9519b6055d1f
Tim <50641103+tikhusaenov@users.noreply.github.com>
2020-11-07 08:27:19 +0300

    added lazy load and loaders

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React, { useEffect, useState } from 'react'
-import TodoList from "./Todo/TodoList/TodoList";
-import Context from "./context";
-import AddTodo from "./Todo/AddTodo/AddTodo";
+import React, { useEffect, useState, lazy} from 'react'
+import TodoList from "./Todo/TodoList/TodoList"
+import Context from "./context"
+import Loader from "./Loader/Loader"
+
+const AddTodo = lazy(() => new Promise(resolve => {
+    setTimeout(() => {
+        resolve(import('./Todo/AddTodo/AddTodo'))
+    },900)
+}))
+
 
 
 function App() {
   const [todos, setTodos] = useState([])
+  const [loader, setLoader] = useState(true)
+
 
   useEffect(() => {
       fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
           .then(response => response.json())
-          .then(todos => setTimeout(() => setTodos(todos),2000  ))
+          .then(todos => setTimeout(() => {
+              setTodos(todos)
+              setLoader(false)
+          },800  ))
   },[])
 
   function toggleTodo(id) {
@@ -51,8 +63,11 @@ function App() {
               <div className="headingList d-flex">
                   <h1>React Tutorial</h1>
               </div>
-              <AddTodo onCreate={ addTodo }/>
-              {todos.length ? <TodoList todos={ todos }/> : <p className="colorGrey">No todos</p>}
+              <React.Suspense fallback={<p>Loading...</p>}>
+                  <AddTodo onCreate={ addTodo }/>
+              </React.Suspense>
+              {loader && <Loader />}
+              {todos.length ? <TodoList todos={ todos }/> : loader ? null : <p className="colorGrey">No todos</p>}
 
           </div>
       </Context.Provider>
---
+++ src/Loader/Loader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Loader.css'
+
+function Loader() {
+    return (
+        <div className="d-flex m-05">
+            <div className="lds-dual-ring"/>
+        </div>
+
+    )
+}
+
+
+export default Loader
